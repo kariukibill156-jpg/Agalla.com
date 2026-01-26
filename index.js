782cf0b5c5e59f914f67ea0b6f1e08cc955886f0
kariukibill156-jpg <kariukibill156@gmail.com>
2026-01-26 22:25:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function scrollToContact(){
-document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+  document.getElementById("contact").scrollIntoView({behavior:"smooth"});
 }
 
-const sections=document.querySelectorAll(".fade-in");
-window.addEventListener("scroll",()=>{
-sections.forEach(sec=>{
-const pos=sec.getBoundingClientRect().top;
-if(pos<window.innerHeight-100){
-sec.classList.add("show");
-}
+const sections = document.querySelectorAll(".fade-in");
+
+/* SHOW SECTIONS ON PAGE LOAD */
+window.addEventListener("load", () => {
+  sections.forEach(sec => {
+    sec.classList.add("show");
+  });
 });
+
+/* ANIMATE ON SCROLL */
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const pos = sec.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      sec.classList.add("show");
+    }
+  });
 });
